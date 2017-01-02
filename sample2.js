760d9a9ba2fd2b9d4875811ededae2d52c82a8fa
vin-droid <vineet.12cs069@abes.ac.in>
2017-01-02 08:40:10 +0530

    another js practice

--- sample2.js
+++ sample2.js
@@ -1,26 +1,48 @@
 
 window.onload = function(){
 	init();
+	document.getElementById('registration').onsubmit = function  () {
+		var name = this.elements[0].value;
+		var age =  this.elements[1].value;
+
+		this.validate = function () {
+			if(name.trim() == "")
+			{
+				alert("Please Enter Your Name");
+				return false;
+			}
+			else if(age < 18 || age.value > 90){
+				alert("Please Enter Your Correct Age");
+				return false;
+			}
+			else{
+				var customer = Customer(name,age);
+				alert("customer created");
+			}
+		};
+	};
 }
 
 
 
-function Customer(name,age ){
 
+
+
+function Customer(name,age ){
 	this.name = name;
 	this.age = age;
 	this.bal = 0.0;
 	this.purchased_items = [];
 	this.itemBucket =[];
 	this.creditBy = function(amount){
-      this.bal += amount;
+		this.bal += amount;
 	}
 	this.debitBy = function(amount){
-      this.bal -= amount;
+		this.bal -= amount;
 	}
 	this.purchaseItems = function(item){
 		var total_purchase = 0.0;
-        var customer_bucket = this.itemBucket;
+		var customer_bucket = this.itemBucket;
 		if(this.itemBucket.length == 0){
 			this.itemBucket.push(item);
 			this.purchaseItems(item);
@@ -28,19 +50,19 @@ function Customer(name,age ){
 		else{
 			this.itemBucket.forEach(function(item) {
 
-                total_purchase += item.price; 
+				total_purchase += item.price; 
 				customer_bucket.pop(item);
 			});
 		}
 		if (this.bal >= total_purchase){
-         this.debitBy(total_purchase);
+			this.debitBy(total_purchase);
 		}
 		else{
 			alert("Please Credit Your Card!  " + this.bal.toPrecision(2) + "$");
 		}
 	}
 	this.addToCart = function(item){
-      this.itemBucket.push(item);
+		this.itemBucket.push(item);
 	}
 }
 
@@ -48,8 +70,6 @@ function Item(name,price){
 	this.name = name;
 	this.price = price;
 }
-
-
 
 function init(){
 	customer1  =  new Customer('vineet',24);
